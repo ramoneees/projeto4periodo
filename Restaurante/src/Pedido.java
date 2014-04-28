2c706754e2c3e6a487c3e81864a285c9c5149bb9
ramonp.rios <ramonp.rios@gmail.com>
2014-04-28 16:54:37 +0000

    Nova Pasta

---
+++ Restaurante/src/Pedido.java
@@ -0,0 +1,58 @@
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@ManyToMany
+	private Collection<Item> itens;
+	
+	@ManyToMany
+	private Collection<Combo> combos;
+	
+	@ManyToOne
+	private Cliente cliente;
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Collection<Item> getItens() {
+		return itens;
+	}
+
+	public void setItens(Collection<Item> itens) {
+		this.itens = itens;
+	}
+
+	public Collection<Combo> getCombos() {
+		return combos;
+	}
+
+	public void setCombos(Collection<Combo> combos) {
+		this.combos = combos;
+	}
+	
+}
---
+++ Restaurante/src/teste.java
@@ -0,0 +1,26 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+
+public class teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Obtendo o gerenciador de entidades
+		EntityManagerFactory emf = Persistence.
+		createEntityManagerFactory("unitPSC");
+		EntityManager em = emf.createEntityManager();
+		// Abrindo transações
+		EntityTransaction et = em.getTransaction();
+		et.begin();
+		// Instanciar objeto para inserir
+	
+		et.commit();
+		// Fechando o gerenciador.
+		em.close(); 
+		emf.close();
+	}
+
+}
